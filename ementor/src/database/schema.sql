11123b15227b71daa2297b06bbec30e38490c8f6
joao santos <linkmagnofxd@gmail.com>
2026-07-10 14:33:19 -0300

    Feat : adição do banco de dados

---
+++ ementor/src/database/schema.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Linux (x86_64)
+--
+-- Host: localhost    Database: ementor_plus
+-- ------------------------------------------------------
+-- Server version	8.0.46-0ubuntu0.24.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `cpf_pessoa` char(11) NOT NULL,
+  `matricula` bigint NOT NULL,
+  `periodo` int NOT NULL,
+  `codigo_turma` varchar(20) NOT NULL,
+  `nota1` decimal(4,2) DEFAULT NULL,
+  `nota2` decimal(4,2) DEFAULT NULL,
+  `nota3` decimal(4,2) DEFAULT NULL,
+  `nota4` decimal(4,2) DEFAULT NULL,
+  `nota5` decimal(4,2) DEFAULT NULL,
+  `nota6` decimal(4,2) DEFAULT NULL,
+  `nota7` decimal(4,2) DEFAULT NULL,
+  `nota8` decimal(4,2) DEFAULT NULL,
+  `nota9` decimal(4,2) DEFAULT NULL,
+  `nota10` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`matricula`),
+  KEY `cpf_pessoa` (`cpf_pessoa`),
+  KEY `codigo_turma` (`codigo_turma`),
+  CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`cpf_pessoa`) REFERENCES `pessoa` (`cpf`),
+  CONSTRAINT `aluno_ibfk_2` FOREIGN KEY (`codigo_turma`) REFERENCES `turma` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `egresso`
+--
+
+DROP TABLE IF EXISTS `egresso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `egresso` (
+  `profissao_atual` varchar(100) NOT NULL,
+  `faixa_salarial` decimal(10,2) DEFAULT NULL,
+  `curso_anterior` varchar(100) DEFAULT NULL,
+  `curso_atual` varchar(100) DEFAULT NULL,
+  `cpf_pessoa` char(11) NOT NULL,
+  KEY `fk_egresso_pessoa` (`cpf_pessoa`),
+  CONSTRAINT `fk_egresso_pessoa` FOREIGN KEY (`cpf_pessoa`) REFERENCES `pessoa` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pessoa`
+--
+
+DROP TABLE IF EXISTS `pessoa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoa` (
+  `cpf` char(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `nascimento` date NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `rua` varchar(100) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` char(2) DEFAULT NULL,
+  PRIMARY KEY (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `professor`
+--
+
+DROP TABLE IF EXISTS `professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor` (
+  `cpf_pessoa` char(11) NOT NULL,
+  `data_admissao` date NOT NULL,
+  `cargo_chefia` tinyint(1) DEFAULT NULL,
+  `cargo_coordenacao` tinyint(1) DEFAULT NULL,
+  `salario_bruto` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`cpf_pessoa`),
+  CONSTRAINT `professor_ibfk_1` FOREIGN KEY (`cpf_pessoa`) REFERENCES `pessoa` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `turma`
+--
+
+DROP TABLE IF EXISTS `turma`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turma` (
+  `codigo` varchar(20) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_usuario` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `nivel_acesso` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-10 14:32:13
